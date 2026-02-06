8b5f19ecc93381660786637abe8d03067d6e83f7
iamsanaullah <119261585+iamsanaullah@users.noreply.github.com>
2026-02-07 03:23:59 +0500

    Origins Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://aykaystaskmanagerbackend.onrender.com/'
+  'https://aykaystaskmanagerbackend.onrender.com'
 ];
 
 app.use(cors({
